bd314182aba91a87dc039bafd1a260518102f53b
edgering <jirka@edgering.org>
2016-05-15 23:54:11 +0200

    Update expander.js

--- expander.js
+++ expander.js
@@ -4,29 +4,45 @@ jQuery.fn.expander = function(){
   // default values
   
   var MaxHeight = (me.attr("data-height")) ? me.data("height") : "5em",
-      lblView = (me.data("view")) ? me.data("view") : "view all",
-      lblHide = (me.data("hide")) ? me.data("hide") : "hide me";
-  
-  // expand button
-  
-  var btnExists = me.find(".btnExpander"),
-      expander = (btnExists.length) ? btnExists : $('<a href="#" class="btnExpander"></a>');
+        lblView = (me.data("view")) ? me.data("view") : "view all",
+        lblHide = (me.data("hide")) ? me.data("hide") : "hide me";
       
-  expander.click(function(){                     
-    if (me.hasClass("stretched"))
-    {
-      me.removeClass("stretched").css("maxHeight","800em");
-      expander.html(lblHide);
-    }
-    else
-    {
-      me.addClass("stretched").css("maxHeight",MaxHeight);
-      expander.html(lblView);
-    }                              
-  });
-
-  // check if height is already limited (ie. in css for better rendering)
-  // if not - do it
+  // do we need it? -> compare MaxHeight with height of a child
   
-  var child = me.children().height(); 
+  var child = me.children().height(),
+         mh = parseInt(MaxHeight);
+    
+  if (MaxHeight.search(/em/i) > 0)
+  {    
+    mh = (mh * parseInt(me.css("fontSize"))); 
+  }
+  
+  if (child > mh)
+  {            
+    // expand button
+  
+    var btnExists = me.find(".btnExpander"),
+         expander = (btnExists.length) ? btnExists : $('<a href="#" class="btnExpander"></a>');
+      
+    expander.click(function(){                     
+      if (me.hasClass("stretched"))
+      {
+        me.removeClass("stretched").css("maxHeight","800em");
+        expander.html(lblHide);
+      }
+      else
+      {
+        me.addClass("stretched").css("maxHeight",MaxHeight);
+        expander.html(lblView);
+      }                              
+    });
+
+    expander.appendTo(me);
+    me.addClass("hasBtnExpander");
+    
+    if (child > me.height())
+    {    
+      expander.trigger("click");
+    }   
+  } 
 }
