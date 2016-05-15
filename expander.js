458a1452f2d41745a30bcd9ff1dd7203debfb55d
edgering <jirka@edgering.org>
2016-05-15 23:33:23 +0200

    Update expander.js

--- expander.js
+++ expander.js
@@ -1,31 +1,32 @@
-jQuery.fn.WrapMe = function(){
-
-  var me = this.attr("id") ? $("#" + this.attr("id")) : this,  
-    temp = me.find("btnExpander").length;
+jQuery.fn.expander = function(){
+  var me = this.attr("id") ? $("#" + this.attr("id")) : this;
   
-  var mh = me.attr("data-height") ? me.data("height") : "10em",
-      lbl_vse = (me.data("vse")) ? me.data("vse") : "vše",
-      lbl_skryt = (me.data("skryt")) ? me.data("skryt") : "skrýt"; 
-                 
-  if (me.children().height() > me.height())
-  {
-    me.addClass("stretch");
-  }
-    
-  var expander = (temp) ? temp : $('<a href="#" class="btnExpander">vše</a>');
-                                        
+  // default values
+  
+  var MaxHeight = (me.attr("data-height")) ? me.data("height") : "5em",
+      lblView = (me.data("view")) ? me.data("view") : "view all",
+      lblHide = (me.data("hide")) ? me.data("hide") : "hide me";
+  
+  // expand button
+  
+  var btnExists = me.find(".btnExpander"),
+      expander = (btnExists.length) ? btnExists : $('<a href="#" class="btnExpander"></a>');
+      
   expander.click(function(){                     
-    if (me.hasClass("stretch"))
+    if (me.hasClass("stretched"))
     {
-      me.removeClass("stretch").css("maxHeight","800em");
-      expander.html(lbl_skryt);
+      me.removeClass("stretched").css("maxHeight","800em");
+      expander.html(lblHide);
     }
     else
     {
-      me.addClass("stretch").css("maxHeight",mh);
-      expander.data(lbl_vse);
+      me.addClass("stretched").css("maxHeight",MaxHeight);
+      expander.html(lblView);
     }                              
   });
-    
-  expander.appendTo(me);    
+
+  // check if height is already limited (ie. in css for better rendering)
+  // if not - do it
+  
+  var child = me.children().height(); 
 }
